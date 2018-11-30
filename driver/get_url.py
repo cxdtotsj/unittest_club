f2f78551031452a798ad932d45bbc8165b42d0f0
cxdtotsj <cxdtotsj@163.com>
2018-11-30 18:20:41 +0800

    V1

---
+++ driver/get_url.py
@@ -0,0 +1,26 @@
+'''
+user:
+data:
+note:
+'''
+
+
+
+
+# 俱乐部——线上
+url ="http://www.styd.cn/"
+# username = "15618157701"
+# password = "styd8817jyjy"
+username = "18317026527"
+password = "tsj123456"
+
+
+# # 俱乐部——Pre预发
+# url ="https://www.pre.styd.cn/"
+# username = "18621696393"
+# password = "123456"
+
+
+
+
+
